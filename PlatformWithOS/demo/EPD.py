59980d60f88501893be6384fb42dfd53d404cb71
Glyn Hudson <glyn.hudson@gmail.com>
2015-05-12 22:01:32 +0100

    Update EPD.py

--- PlatformWithOS/demo/EPD.py
+++ PlatformWithOS/demo/EPD.py
@@ -136,8 +136,8 @@ to use:
             raise EPDError('image size mismatch')
 
         with open(os.path.join(self._epd_path, 'LE', 'display_inverse'), 'r+b') as f:
-	        image = image.rotate(180) # Flip the image round 180 degrees - upside down	
-            f.write(image.tostring())
+        	image = image.rotate(180) # Flip the image round 180 degrees - upside down	
+        	f.write(image.tostring())
 
         if self.auto:
             self.update()
